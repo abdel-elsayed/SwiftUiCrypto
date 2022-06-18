cfb9af073fa3625fd479dc564fe3fd210c2d15da
abdel-elsayed <abdel.elsayed18@gmail.com>
2022-06-18 11:43:41 -0400

    Initial Commit

---
+++ SwiftuiCrypto/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftuiCrypto
+//
+//  Created by Abdelrahman Elsayed on 6/18/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftuiCrypto/SwiftuiCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftuiCryptoApp.swift
+//  SwiftuiCrypto
+//
+//  Created by Abdelrahman Elsayed on 6/18/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftuiCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
